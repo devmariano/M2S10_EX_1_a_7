7ef7c37e56e29fc5ecc4afd8a238250cc5806160
Alexandre Mariano <86934710+devmariano@users.noreply.github.com>
2023-07-09 13:09:06 -0300

    EX03 - Alterar CardProduto para StyledComponents

--- src/components/CardProduto/CardProduto.jsx
+++ src/components/CardProduto/CardProduto.jsx
@@ -1,35 +1,41 @@
-import './CardProduto.css';
+import React from 'react';
 import PropTypes from 'prop-types';
 import DetalhesProdutoModal from '../DetalhesProdutoModal/DetalhesProdutoModal';
+import { ProductCard, ImageWrapper, Image, Price, CardData, Title, Description, CardDetails, List, ListItem, DetailsButton, ImgItem } from './CardProdutoStyles';
+import icon from '../../assets/icon.png';
 
 function CardProduto(props) {
   const { foto, preço, nome, descricao, caracteristicas } = props;
 
   return (
-    <div className="product-card">
-      <div className="image-wrapper">
-        <img src={foto} alt="Capa do curso" />
-        <span>{preço}</span>
-      </div>
-      <div className="card-data">
-        <h2>{nome}</h2>
-        <p>{descricao}</p>
-        <div className="card-details">
-          <div className="details">
-            <ul>
-              {caracteristicas.map((item, index) => {
-                if (item) {
-                  return <li key={index}>{item}</li>;
-                }
-              })}
-            </ul>
-          </div>
-          <div className="details-button">
-            <DetalhesProdutoModal foto={foto} preço={preço} nome={nome} descricao={descricao} caracteristicas={caracteristicas}/>
-          </div>
-        </div>
-      </div>
-    </div>
+    <ProductCard>
+      <ImageWrapper>
+        <Image src={foto} alt="Capa do curso" />
+        <Price>{preço}</Price>
+      </ImageWrapper>
+      <CardData>
+        <Title>{nome}</Title>
+        <Description>{descricao}</Description>
+        <CardDetails>
+          <List>
+            {caracteristicas.map((item, index) => {
+              if (item) {
+                return <ListItem key={index}><ImgItem src={icon}/>{item}</ListItem>;
+              }
+            })}
+          </List>
+          <DetailsButton>
+            <DetalhesProdutoModal
+              foto={foto}
+              preço={preço}
+              nome={nome}
+              descricao={descricao}
+              caracteristicas={caracteristicas}
+            />
+          </DetailsButton>
+        </CardDetails>
+      </CardData>
+    </ProductCard>
   );
 }
 
